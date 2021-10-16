e1433ce169e13884048e1c303dddc4e2484e6846
AleksDu <aleksandr.dyshkin@gmail.com>
2021-10-16 19:18:45 +0300

    Change Contacts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import Container from "./components/Container";
+import ContactForm from "./components/ContactForm";
+import Filter from "./components/Filter";
+import ContactList from "./components/ContactList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import s from "./App.module.css";
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+    if (nextContacts !== prevContacts) {
+      localStorage.setItem("contacts", JSON.stringify(nextContacts));
+    }
+  }
+
+  // Добавляет контакт
+  addContact = (data) => {
+    const normalizedName = data.name.toLowerCase();
+    const uniqId = Date.now().toString();
+
+    // Создает новый контакт с айди из даты
+    const newContact = {
+      id: uniqId,
+      name: normalizedName,
+      number: data.number,
+    };
+    // Проверка на дубликат
+    const duplicateName = this.state.contacts.find(
+      (contact) => contact.name === newContact.name
+    );
+
+    if (duplicateName) {
+      alert(`${newContact.name} is already on contacts`);
+      return;
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }));
+  };
+
+  // Следит за полем фильтрации и пишет в стейт
+  changeFilter = (event) => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  // Фильтрует и возвращает результат фильтра
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
+
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  // Удаляет контакт
+  deleteContact = (id) => {
+    const answer = window.confirm("Do you want to delete?");
+    if (answer) {
+      this.setState((prevState) => ({
+        contacts: prevState.contacts.filter((contact) => contact.id !== id),
+      }));
+    }
+  };
+
+  render() {
+    const { filter } = this.state;
+    const filteredResults = this.filterContacts();
+
+    return (
+      <Container>
+        <h1 className={s.title}>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2 className={s.title}>Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={filteredResults}
+          onDeleteContact={this.deleteContact}
+        />
+      </Container>
+    );
+  }
 }
 
 export default App;
